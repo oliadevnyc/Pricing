f614bc1b8aec50827f9c8422061fcf042969ab1c
Olia <nycdimples1988@gmail.com>
2016-09-17 14:41:52 -0400

    requiredvalidate fix

--- app/models/rod.rb
+++ app/models/rod.rb
@@ -1,5 +1,4 @@
 class Rod < ActiveRecord::Base
-  validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
   has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100#" },
       :default_url => "/images/:style/missing.png",
       :url  => ":s3_domain_url",
@@ -11,4 +10,7 @@ class Rod < ActiveRecord::Base
           aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"]
       },
       fog_directory: ENV["FOG_DIRECTORY"]
+
+  validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
+
 end
