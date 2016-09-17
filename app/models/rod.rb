d3d520602cbbe38d6b13fb5d0a736325155fc753
Olia <nycdimples1988@gmail.com>
2016-09-17 14:34:54 -0400

    requiredvalidate

--- app/models/rod.rb
+++ app/models/rod.rb
@@ -1,5 +1,5 @@
 class Rod < ActiveRecord::Base
-  #validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
+  validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
   has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100#" },
       :default_url => "/images/:style/missing.png",
       :url  => ":s3_domain_url",
